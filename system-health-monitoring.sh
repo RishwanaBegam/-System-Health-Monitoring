a1a4228fadb8a6ce6c1a3e101e30bc91c1914fe5
RishwanaBegam <149790091+RishwanaBegam@users.noreply.github.com>
2024-07-20 14:36:53 +0530

    Create system-health-monitoring.sh

---
+++ system-health-monitoring.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+#############################
+
+CPU_THRESHOLD=80
+MEMORY_THRESHOLD=80
+DISK_THRESHOLD=80
+LOG_FILE="/var/log/health.log"
+
+
+check_cpu_usage() {
+  cpu_usage=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+  echo "CPU usage: ${cpu_usage}%"
+  if (( $(echo "$cpu_usage > $CPU_THRESHOLD" | bc -l) )); then
+    echo "ALERT: CPU usage is above threshold: ${cpu_usage}%" | tee -a $LOG_FILE
+  fi
+}
+
+
+check_memory_usage() {
+  memory_usage=$(free | grep Mem | awk '{print $3/$2 * 100.0}')
+  echo "Memory usage: ${memory_usage}%"
+  if (( $(echo "$memory_usage > $MEMORY_THRESHOLD" | bc -l) )); then
+    echo "ALERT: Memory usage is above threshold: ${memory_usage}%" | tee -a $LOG_FILE
+  fi
+}
+
+
+check_disk_space() {
+  disk_usage=$(df -h / | grep / | awk '{print $5}' | sed 's/%//')
+  echo "Disk usage: ${disk_usage}%"
+  if [ $disk_usage -gt $DISK_THRESHOLD ]; then
+    echo "ALERT: Disk usage is above threshold: ${disk_usage}%" | tee -a $LOG_FILE
+  fi
+}
+
+
+check_running_processes() {
+  process_count=$(ps aux | wc -l)
+  echo "Running Processes are: ${process_count}"
+}
+
+#functions
+check_cpu_usage
+check_memory_usage
+check_disk_space
+check_running_processes
+
